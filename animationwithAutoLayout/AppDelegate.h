d5c8b083ee3a1c846828fcefe69d35f276e20229
Wei TingTao <s952865@go.thu.edu.tw>
2015-12-16 09:53:47 -0600

    Initial Commit

---
+++ animationwithAutoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animationwithAutoLayout
+//
+//  Created by Wei TingTao on 12/16/15.
+//  Copyright © 2015 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
